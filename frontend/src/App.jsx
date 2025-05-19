2f0634b2aa74dca8dcf2c4af1e10edfdc2847887
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2025-05-19 17:04:23 +0530

    feat: add routing and new pages to the frontend application
    - Updated package.json to include react-hot-toast and react-router dependencies.
    - Implemented routing in App.jsx with new pages: HomePage, SignUpPage, LoginPage, ChatPage, CallPage, OnBoardingPage, and NotificationPage.
    - Created new page components for each route.
    - Wrapped the App component in BrowserRouter for routing functionality.
    - Integrated daisyUI into Tailwind CSS configuration for enhanced styling options.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Routes ,Route} from 'react-router';
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import NotificationPage from './pages/NotificationPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import OnBoardingPage from './pages/OnBoardingPage.jsx';
+
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="bg-red-600 h-screen text-white flex justify-center items-center text-3xl">
-      Tailwind Working ✅
+    <div className="h-screen " data-theme="dracula">
+      
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/call" element={<CallPage />} />
+            <Route path="/onboarding" element={<OnBoardingPage />} /> 
+            <Route path="/notification" element={<NotificationPage />} />
+
+          </Routes>
+          <Toaster/>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,14 @@ import React from 'react'; // ✅ Add this line
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { StrictMode } from 'react';
+
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <StrictMode>
+      <BrowserRouter>
+      <App />
+  </BrowserRouter>
+    </StrictMode>
 );
